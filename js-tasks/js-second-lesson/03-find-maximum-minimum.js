63fda902a00f79c9e53d90f53c0ffaff79937548
maximzink <34608161+maximzink@users.noreply.github.com>
2023-07-22 14:35:01 +0300

    Update 03-find-maximum-minimum.js

---
+++ js-tasks/js-second-lesson/03-find-maximum-minimum.js
@@ -0,0 +1,14 @@
+// Maksym Zinchuk
+var min = function (list) {
+  list.sort(function (a, b) {
+    return a - b;
+  });
+  return list[0];
+};
+
+var max = function (list) {
+  list.sort(function (a, b) {
+    return b - a;
+  });
+  return list[0];
+};
