d2438e5d782b5fa737e1b7f7afc9ef24fe34efff
Slava Eremin <slava.erema@gmail.com>
2023-07-24 15:40:29 +0300

    Update 05-wolf-in-sheep's-clothing.js

--- js-tasks/js-second-lesson/05-wolf-in-sheep's-clothing.js
+++ js-tasks/js-second-lesson/05-wolf-in-sheep's-clothing.js
@@ -1,1 +1,6 @@
+const warnTheSheep = queue => {
+  const wolfIndex = queue.indexOf("wolf");
+  const sheepPosition = queue.length - wolfIndex - 1;
+  return wolfIndex === queue.length - 1 ? "Pls go away and stop eating my sheep" : `Oi! Sheep number ${sheepPosition}! You are about to be eaten by a wolf!`;        
+};
 
