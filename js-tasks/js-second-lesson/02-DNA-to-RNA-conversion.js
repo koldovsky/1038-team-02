1aa56046ef5d496d654da8638720bc76dadb1d70
Slava Eremin <slava.erema@gmail.com>
2023-07-24 12:28:15 +0300

    Update 02-DNA-to-RNA-conversion.js

--- js-tasks/js-second-lesson/02-DNA-to-RNA-conversion.js
+++ js-tasks/js-second-lesson/02-DNA-to-RNA-conversion.js
@@ -14,4 +14,9 @@ function DNAtoRNA(dna) {
   function DNAtoRNA(dna) {
     return dna.replace(/T/g, "U");
   }
-  
+  //Eremin Slava
+function DNAtoRNA(dna) {
+  // create a function which returns an RNA sequence from the given DNA sequence
+  const replaceDNAtoRNA = dna => dna.replace(/T|t/g, "U");
+    return replaceDNAtoRNA(dna);
+}
