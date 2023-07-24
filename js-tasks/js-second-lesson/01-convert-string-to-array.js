5a21844f2f325374040cd9c99c45c17bfaaedcea
Slava Eremin <slava.erema@gmail.com>
2023-07-24 11:45:37 +0300

    Update 01-convert-string-to-array.js

--- js-tasks/js-second-lesson/01-convert-string-to-array.js
+++ js-tasks/js-second-lesson/01-convert-string-to-array.js
@@ -3,3 +3,8 @@ function stringToArray(string) {
     return string.split(" ");
   }
   
+//Eremin Slava
+function stringToArray(string){ 
+  string = string.split(" ").map(word => word.trim());
+  return string;
+}
