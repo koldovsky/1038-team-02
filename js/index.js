63a5899f59b1f6db5f58908fe5be8a048ea61ecc
ConstantinKlius <121198957+ConstantinKlius@users.noreply.github.com>
2023-08-07 20:13:46 +0000

    add id into feedback

--- js/index.js
+++ js/index.js
@@ -1,38 +1,41 @@
 document.addEventListener('partialsLoaded', () => {
-import('./faq.js');
-import('./feedbacks.js')
+  import('./faq.js');
+  import('./feedbacks.js');
+  import('./contacts.js');
 });
 
 // code for the pricing part - Dunhovsky
 
-function pricingSwiper() {const swiper = new Swiper(".slide-content", {
-  slidesPerView: 3,
-  spaceBetween: 25,
-  loop: true,
-  centerSlide: 'true',
-  fade: 'true',
-  grabCursor: 'true',
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    dynamicBullets: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+function pricingSwiper() {
+  const swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
 
-  breakpoints:{
+    breakpoints: {
       0: {
-          slidesPerView: 1,
+        slidesPerView: 1,
       },
       520: {
-          slidesPerView: 2,
+        slidesPerView: 2,
       },
       950: {
-          slidesPerView: 3,
+        slidesPerView: 3,
       },
-  },
-});}
+    },
+  });
+}
 
 setTimeout(pricingSwiper, 1000);
